03a69999cdac49ed13891714b17cf07cac4067bd
Darshan <darshanpatel2923@gmail.com>
2023-09-23 22:35:19 +0530

    Connected to MongoDB and defined schema.

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,13 @@
 import express from 'express'
-
+import mongoose from 'mongoose';
+import cors from 'cors'
+const corapps = cors();
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+// Above to are to handle data coming from frontend
+
+mongoose.connect("mongodb://localhost:27017/userData")
 
 app.listen(5050, () => console.log("Server is running"))
