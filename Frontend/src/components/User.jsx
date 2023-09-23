c73c2baf625d34dfdc6ac9664bd284585142382e
Darshan <darshanpatel2923@gmail.com>
2023-09-23 22:14:03 +0530

    Added design to the home page of the app

--- Frontend/src/components/User.jsx
+++ Frontend/src/components/User.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const User = () => {
     const [users, setUsers] = useState([{
@@ -7,6 +8,7 @@ const User = () => {
     return (
         <div className='d-flex vh-100 bg-success justify-content-center align-items-center'>
             <div className="w-50 bg-white rounded p-3">
+                <Link to='/create' className='btn btn-success'>Add +</Link>
                 <table className="table">
                     <thead>
                         <tr>
@@ -18,7 +20,14 @@ const User = () => {
                     </thead>
                     <tbody>
                         {
-                            users.map
+                            users.map((user) => (
+                                <tr>
+                                    <td>{user.Name}</td>
+                                    <td>{user.Email}</td>
+                                    <td>{user.Age}</td>
+                                    <td><button>Edit</button><button>Delete</button></td>
+                                </tr>
+                            ))
                         }
                     </tbody>
                 </table>
