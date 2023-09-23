511a71c3dc71f48f49a2464cfa025aff52f166d3
Darshan <darshanpatel2923@gmail.com>
2023-09-23 22:22:51 +0530

    Added CreateUser and UpdateUser pages to project.

--- Frontend/src/components/Create.jsx
+++ Frontend/src/components/Create.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const Create = () => {
     return (
-        <div>
-
+        <div className='d-flex vh-100 bg-success justify-content-center align-items-center'>
+            <div className="w-50 bg-white rounded p-3">
+                <form>
+                    <h2>Add User</h2>
+                    <div className="mb-2">
+                        <label htmlFor="">Name</label>
+                        <input type="text" placeholder='Enter The Name' className='form-control' />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="">Email</label>
+                        <input type="email" placeholder='Enter The Email' className='form-control' />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="">Age</label>
+                        <input type="text" placeholder='Enter The Age' className='form-control' />
+                    </div>
+                    <button className="btn btn-success">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
