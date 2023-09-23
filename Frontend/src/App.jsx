7ddfdbf3bc792f5cb62340b65e06fc93fa6f73db
Darshan <darshanpatel2923@gmail.com>
2023-09-23 20:17:30 +0530

    Added all components required for project.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import User from './components/User'
+import Create from './components/Create'
+import Update from './components/Update'
 
 const App = () => {
   return (
-    <>
-
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<User />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/update" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
