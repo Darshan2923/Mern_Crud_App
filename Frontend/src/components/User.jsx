8e57824c73d8163384d1ecda3fa08e2450474205
Darshan <darshanpatel2923@gmail.com>
2023-09-23 20:30:59 +0530

    Added content to app.jsx

--- Frontend/src/components/User.jsx
+++ Frontend/src/components/User.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const User = () => {
+    const [users, setUsers] = useState([{
+        Name: "chiku", Email: "ch@gmail.com", Age: 18
+    }]);
     return (
-        <div>User</div>
+        <div className='d-flex vh-100 bg-success justify-content-center align-items-center'>
+            <div className="w-50 bg-white rounded p-3">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     )
 }
 
