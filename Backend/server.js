f4b71604e6740af3524debd025344dbc5942d66d
Darshan <darshanpatel2923@gmail.com>
2023-09-24 14:47:51 +0530

    Created route for storing data & added to mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,13 @@ app.use(cors());
 app.use(express.json());
 // Above to are to handle data coming from frontend
 
-mongoose.connect("mongodb://localhost:27017/userData")
+mongoose.connect("mongodb://127.0.0.1:27017/userData")
+
+//New Record API
+app.post('/createUser', (req, res) => {
+    UserModel.create(req.body)
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+})
 
 app.listen(5050, () => console.log("Server is running"))
--- Frontend/src/components/Create.jsx
+++ Frontend/src/components/Create.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
+    const [name, SetName] = useState('');
+    const [age, SetAge] = useState('');
+    const [email, SetEmail] = useState('');
+    const navigate = useNavigate()
+
+    const Submit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:5050/createUser', { name, email, age })
+            .then(result => {
+                console.log(result)
+                SetName('');
+                SetEmail('');
+                SetAge('');
+                navigate('/')
+            })
+            .catch(err => console.log(err))
+    }
     return (
         <div className='d-flex vh-100 bg-success justify-content-center align-items-center'>
             <div className="w-50 bg-white rounded p-3">
@@ -8,17 +27,20 @@ const Create = () => {
                     <h2>Add User</h2>
                     <div className="mb-2">
                         <label htmlFor="">Name</label>
-                        <input type="text" placeholder='Enter The Name' className='form-control' />
+                        <input type="text" placeholder='Enter The Name' className='form-control'
+                            onChange={(e) => SetName(e.target.value)} />
                     </div>
                     <div className="mb-2">
                         <label htmlFor="">Email</label>
-                        <input type="email" placeholder='Enter The Email' className='form-control' />
+                        <input type="email" placeholder='Enter The Email' className='form-control'
+                            onChange={(e) => SetEmail(e.target.value)} />
                     </div>
                     <div className="mb-2">
                         <label htmlFor="">Age</label>
-                        <input type="text" placeholder='Enter The Age' className='form-control' />
+                        <input type="text" placeholder='Enter The Age' className='form-control'
+                            onChange={(e) => SetAge(e.target.value)} />
                     </div>
-                    <button className="btn btn-success">Submit</button>
+                    <button className="btn btn-success" onClick={Submit}>Submit</button>
                 </form>
             </div>
         </div>
